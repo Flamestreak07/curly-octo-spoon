423ec283a0866f63a949c0aed53521979746aebf
albunde <140499389+albunde@users.noreply.github.com>
2024-03-16 11:35:25 +1300

    topdown work
    moved some stuff, added assets, and begun work on the common topdown control scheme system

---
+++ curly-octo-spoon/Assets/Scripts/Common/TopdownControlSchemeCommon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownControlSchemeCommon : MonoBehaviour
+{
+    [SerializeField] public bool ManipulationEnabled = true;
+    [SerializeField] public bool MovementEnabled = true;
+    [SerializeField] public float tentacleStretchModifier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
