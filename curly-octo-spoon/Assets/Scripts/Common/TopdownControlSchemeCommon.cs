543e2978cc8bb66cf2b4164ba03936b0b9a11879
albunde <140499389+albunde@users.noreply.github.com>
2024-03-17 12:00:50 +1300

    Frameworking for TD control scheme
    Some basic frameworking, no visible gameplay changes
    
    Mostly committing now to get progress onto repo in case i need to revert later.
    
    Spent more time than i would have liked today trying to figure out how i should get this to wor

--- curly-octo-spoon/Assets/Scripts/Common/TopdownControlSchemeCommon.cs
+++ curly-octo-spoon/Assets/Scripts/Common/TopdownControlSchemeCommon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TopdownControlSchemeCommon : MonoBehaviour
@@ -7,16 +8,39 @@ public class TopdownControlSchemeCommon : MonoBehaviour
     [SerializeField] public bool ManipulationEnabled = true;
     [SerializeField] public bool MovementEnabled = true;
     [SerializeField] public float tentacleStretchModifier = 1;
-    [SerializeField] GameObject[] tentacles;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject[] Tentacles;
+
+    // Declaration of gameobject storage arrays
+    // i was going to use a 2d array but they're such a nightmare to work with once you enter the realm of actually having to write the code you designed so i was like "nah fuck it"
+    [SerializeField] GameObject[] BackPieces;
+    [SerializeField] GameObject[] MidPieces;
+    [SerializeField] GameObject[] EndPieces;
+
+    // Monobehaviours
+
+    private void Awake() 
+    {
+
+    }
+
+    private void Start() 
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
         
     }
+
+    // Local Functions
+
 }
+
+/* 
+THINGS TO DO:
+    - Make basic tentacle stretching and shit. also functions for them
+    - Make code for moving around (technical design nightmare)
+    - Make code for manual tentacle manipulation (less nightmarish)
+    - Polish and better input support (way easier. hopefully)
+*/
