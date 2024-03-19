ee25051d279a4f914875c66d7fc572c84c913d10
albunde <140499389+albunde@users.noreply.github.com>
2024-03-19 16:41:47 +1300

    added note regarding networking
    Added a note about preparing for netcode integration
    
    As for the progress on the topdown scheme, I'm nearly done with the technical design and will probably begin work on it tomorrow

--- curly-octo-spoon/Assets/Scripts/FloppyRenderer.cs
+++ curly-octo-spoon/Assets/Scripts/FloppyRenderer.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/* IMPORTANT NOTE ABOUT NETWORKING:
+    In order for the whole networking thing to work, different instances of the player
+    octopus, *of the same blueprint* must be able to be "possessed" by a client each -
+    there will not be a separate "ghost octo" type that visually and mechanically
+    represents other players
+
+    At least, if i recall how Netcode for gameobjects works
+
+    Basically, just keep in mind that octopi should not be affected by inputs by
+    default; maybe have like a bool or something that enables input and "assignment
+    to a player" that you have defaulted to true for testing for now
+
+    anyways thats all, have a nice day :thumbsup:
+*/
 public class FloppyRenderer : MonoBehaviour
 {
     [SerializeField] private LineRenderer _renderer;
